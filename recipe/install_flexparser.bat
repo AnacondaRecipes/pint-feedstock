920c044a4658b6659667885ef33e4f3589cf64df
Andy Maloney <amaloney@mailbox.org>
2026-09-18 11:57:30 -0500

    add windows scripts

---
+++ recipe/install_flexparser.bat
@@ -0,0 +1,3 @@
+cd flexparser
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+if errorlevel 1 exit 1
---
+++ recipe/install_pint.bat
@@ -0,0 +1,3 @@
+cd pint
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+if errorlevel 1 exit 1
